d9931cd3915087b064b886d8a4057c0c152cbab3
s3xyRobot <ybtech.chanel@gmail.com>
2025-03-06 18:20:43 +0100

    Update global_variables.cpp

--- global_variables.cpp
+++ global_variables.cpp
@@ -1,4 +1,4 @@
-// vectors.cpp
+// global_variables
 #include <iostream>
 #include <vector>
 #include "global_variables.h"
@@ -10,10 +10,10 @@ double TI_sum{0};
 double ATS{0};
 double IPP{0};
 double TMA{0};
-std::vector<int> TAB(n, 0);;
+std::vector<int> TAB(n, 0);//redimension the vector 
 std::vector<int> AT(n, 0);    
-std::vector<int> DO(n, 0);;
-std::vector<int> SO(n, 0);//redimmensionne le vecteur       
+std::vector<int> DO(n, 0);
+std::vector<int> SO(n, 0);      
 std::vector<int> EO(n, 0);    
 std::vector<int> TS(n, 0);      
 std::vector<int> TI(n, 0);    
